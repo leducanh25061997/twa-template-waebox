f93dceafba697c1fd3506cdd1731b6a912c6074c
leducanh25061997 <leducanhqp@gmail.com>
2024-06-27 16:57:05 +0700

    feature: init src

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 // this manifest is used temporarily for development purposes
-const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+// const manifestUrl =
+//   "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
+  <TonConnectUIProvider manifestUrl={"https://leducanh25061997.github.io/twa-template-waebox/tonconnect-manifest.json"}>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
+  base: '/twa-template-waebox/',
+  // base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
 });
